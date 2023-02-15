e78d361c4a7041899488063e1b0a37caf49c2478
CLeeBenjamin <castonboyd@gmail.com>
2023-02-15 12:59:24 -0500

    updated config

--- dbconfig.js
+++ dbconfig.js
@@ -2,18 +2,19 @@ const { Pool } = require('pg')
 
 const connectionString = 'postgresql://postgres:f1ynjwcZwvCVSbPqlFAH@containers-us-west-32.railway.app:7290/railway'
 
-const developmentPool = new Pool({
-  connection: {
+const developmentPool = new Pool(
+  {
     database: 'todo4',
     user:     'postgres',
     password: ''
-  }
- })
+  })
  
  
  const productionPool = new Pool({
   connectionString
  })
  
+
+const pool = (process.env.NODE_ENV === "production" ? productionPool: developmentPool)
  
- module.exports = productionPool
+module.exports = pool
